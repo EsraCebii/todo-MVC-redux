c7f4c783a361a85d6c67bf4b612bb72dc93daba5
Esra Cebi <esracebi.81@gmail.com>
2021-08-31 19:31:43 +0300

    yeni todo eklemek

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import {addTodo} from '../redux/todos/todosSlice';
+import {nanoid} from '@reduxjs/toolkit';
+
 
 function Form() {
+	const [title, setTitle]= useState('');
+
+	const dispatch = useDispatch();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		dispatch(addTodo({id:nanoid(), title, completed: false}))
+
+	};
     return (
-		<form>
-			<input className="new-todo" placeholder="What needs to be done?" autoFocus />
+		<form onSubmit={handleSubmit}>
+			<input 
+			className="new-todo" 
+			placeholder="What needs to be done?" autoFocus 
+			value={title}
+			onChange={(e) => setTitle(e.target.value)} />
 		</form>
     )
 }
 
-export default Form
+export default Form;
